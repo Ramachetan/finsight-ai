2d8f81c2341818d3ea966d111e529e966c0d7632
Rama Chetan Atmudi <ratmudi@ford.com>
2025-12-22 07:23:44 -0500

    refactor: remove unnecessary React import from ProcessingProgress component

--- frontend/components/ProcessingProgress.tsx
+++ frontend/components/ProcessingProgress.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getProcessingStatus } from '../lib/api';
 
 interface ProcessingProgressProps {
